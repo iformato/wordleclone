0ba8fdb1027d8bfae6b736ee98b000aaf1b22bce
iformato <isaacformato@gmail.com>
2022-07-19 22:59:12 -0400

    first commit of this wordle clone

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String word = "plank";
+
+
+            while (true)
+            {
+                Console.WriteLine("Please, guess the word!");
+                string Guess = Console.ReadLine();
+                foreach (char i in Guess)
+                {
+                    Console.WriteLine(i);
+                    Console.WriteLine(Guess.IndexOf(i));
+                    Console.WriteLine(word.IndexOf(i));
+                    if (word.Contains(i))
+                    {
+                        if (Guess.IndexOf(i).Equals(word.IndexOf(i)))
+                        {
+                            Console.WriteLine(i + " is correct!");
+                        }
+                        else
+                        {
+                            Console.WriteLine(i + " is in the word, but not that spot");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine(i + " is incorrect!");
+                        continue;
+                    }
+                }
+
+            }
+        }
+
+
+    }
+}
+
+
